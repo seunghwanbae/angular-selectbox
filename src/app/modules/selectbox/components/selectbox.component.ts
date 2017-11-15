ffb2008f72f67ddb6e46a9880333117ea0b880ec
seunghwan <shbcaw@nate.com>
2017-11-15 09:24:36 +0900

    create angular cli project

---
+++ src/app/modules/selectbox/components/selectbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selectbox',
+  templateUrl: './selectbox.component.html',
+  styleUrls: ['./selectbox.component.css']
+})
+export class SelectboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modules/selectbox/selectbox.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SelectboxComponent } from './components/selectbox.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SelectboxComponent]
+})
+export class SelectboxModule { }
